c93a424a2da28663dd8ba84cfd925612315481f0
isaiahAndrew <clydewitoutabonnie@gmail.com>
2024-02-14 13:51:27 +0100

    Update main.jsx

--- folders/steps/main.jsx
+++ folders/steps/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// update document title to React Steps
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
