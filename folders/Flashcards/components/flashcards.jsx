e27e0e3460ac89efc500f78dde560cebd1b69bc4
isaiahAndrew <clydewitoutabonnie@gmail.com>
2024-03-04 09:59:24 +0100

    Add files via upload

--- folders/Flashcards/components/flashcards.jsx
+++ folders/Flashcards/components/flashcards.jsx
@@ -1,1 +1,19 @@
+function FlashCards(props) {
+    return (
+        <>
+            <div className="flashcards">
+                {props.questionarray.map(question => (
+                    <div 
+                        key={question.id} 
+                        className={question.id === props.id ? "selected" : ""}
+                        onClick={() => props.clickEvent(question.id)}
+                    >
+                        <p>{question.id === props.id ? question.answer : question.question}</p>
+                    </div>
+                ))} 
+            </div>
+        </>
+    )
+}
 
+export default FlashCards
