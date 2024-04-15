df1c0f992b9e98245039943c2faaa50fa7823687
NetChunin <clydewitoutabonnie@gmail.com>
2024-04-15 11:38:34 +0100

    Add files via upload

--- folders/accordion-part-one/components/accordionItem.jsx
+++ folders/accordion-part-one/components/accordionItem.jsx
@@ -1,1 +1,22 @@
+import { useState } from "react"
 
+/* eslint-disable react/prop-types */
+const AccordionItem = (props) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    function handleToggle() {
+        setIsOpen((cur) => !cur)
+    }
+    return (
+        <>
+            <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+                <p className="number">{props.num < 9 ? `0${props.num + 1}` : props.num + 1}</p>
+                <p className="title">{props.title}</p>
+                <p className="icon">{isOpen ? '-' : '+'}</p>
+                {isOpen && <div className="content-box">{props.text}</div>}
+            </div>
+        </>
+    )
+}
+
+export default AccordionItem
