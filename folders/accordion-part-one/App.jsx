204fec1123c53a1ed03c8e01d8bb513494fd9c97
NetChunin <clydewitoutabonnie@gmail.com>
2024-04-15 11:36:48 +0100

    Add files via upload

--- folders/accordion-part-one/App.jsx
+++ folders/accordion-part-one/App.jsx
@@ -1,1 +1,25 @@
+/* eslint-disable react/prop-types */
+import AccordionItem from "./components/accordionItem"
+import { faqs } from './accdb';
 
+
+function Accordion({data}) {
+  return (
+    <>
+      <div className="accordion">
+        {data.map((el, i) => (
+          <AccordionItem title={el.title} text={el.text} key={el.title} num={i} />
+        ))}
+      </div>
+    </>
+  )
+}
+function App() {
+  return (
+    <>
+      <Accordion data={faqs}/>
+    </>
+  )
+}
+
+export default App
