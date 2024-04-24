5ae2c23f381a7716bda8a4d949142a0fa8a4043e
NetChunin <clydewitoutabonnie@gmail.com>
2024-04-24 14:14:27 +0100

    Add files via upload

--- folders/tip calculator/components/cusTip.jsx
+++ folders/tip calculator/components/cusTip.jsx
@@ -4,7 +4,7 @@ function GetCustomerTip(props) {
         <>
             <label>
                 <span>{props.children}</span>
-                <select value={props.percOne} name="select" onChange={props.onchange}>
+                <select value={props.percentage} name="select" onChange={(e) => props.onSelect(Number(e.target.value))}> 
                     <option value={0}>Dissatisfied (0%)</option>
                     <option value={5}>It was okay (5%)</option>
                     <option value={10}>It was good (10%)</option>
