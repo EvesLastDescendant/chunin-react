c5a1fbfe380b08657ac19b3f0e427bc97dac508a
NetChunin <clydewitoutabonnie@gmail.com>
2024-04-24 14:16:15 +0100

    Delete folders/tip calculator/components/totalprice.jsx

--- folders/tip calculator/components/totalprice.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-function TotalAmt (props) {
-    return (
-        <>
-            {props.amount > 0 && <h3>You pay💲{props.amount + props.tip} {`(💲${props.amount} + 💲${props.tip} tip)`}</h3>}
-        </>
-    )
-}
-export default TotalAmt
