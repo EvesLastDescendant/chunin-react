8d3033cbafcf90d82b27e0543f75305c4df26592
NetChunin <clydewitoutabonnie@gmail.com>
2024-04-24 09:45:13 +0100

    Add files via upload
    lifted up state

--- folders/accordion-part-two/App.jsx
+++ folders/accordion-part-two/App.jsx
@@ -1,1 +1,35 @@
+/* eslint-disable react/prop-types */
+import AccordionItem from "./components/accordionItem"
+import { faqs } from './accdb';
+import { useState } from "react";
 
+
+function Accordion({data}) {
+  const [accordionCurrentlyOpen, setAccordionCurrentlyOpen] = useState(null)
+
+  return (
+    <>
+      <div className="accordion">
+        {data.map((el, i) => (
+          <AccordionItem 
+            accordionCurrentlyOpen={accordionCurrentlyOpen} 
+            onOpen={setAccordionCurrentlyOpen} 
+            title={el.title} 
+            text={el.text} 
+            key={el.title} 
+            num={i} 
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+function App() {
+  return (
+    <>
+      <Accordion data={faqs}/>
+    </>
+  )
+}
+
+export default App
