835de0857b22178c9d0c4e56520c30b753006b0c
NetChunin <clydewitoutabonnie@gmail.com>
2024-04-24 09:47:15 +0100

    Add files via upload

--- folders/accordion-part-two/component/accordionItem.jsx
+++ folders/accordion-part-two/component/accordionItem.jsx
@@ -1,1 +1,20 @@
+/* eslint-disable react/prop-types */
+const AccordionItem = (props) => {
+    const isOpen = props.num === props.accordionCurrentlyOpen
 
+    function handleToggle() {
+        props.onOpen(props.num)
+    }
+    return (
+        <>
+            <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+                <p className="number">{props.num < 9 ? `0${props.num + 1}` : props.num + 1}</p>
+                <p className="title">{props.title}</p>
+                <p className="icon">{isOpen ? '-' : '+'}</p>
+                {isOpen && <div className="content-box">{props.text}</div>}
+            </div>
+        </>
+    )
+}
+
+export default AccordionItem
